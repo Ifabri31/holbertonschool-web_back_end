46bc2659cf5638101b92b77c1acf9210102d2cf7
Fabri <oviedofabrizzio1998@gmail.com>
2024-11-13 10:21:58 -0300

    fix tasks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,35 @@
 module.exports = {
-    env: {
-      browser: true,
-      commonjs: true,
-      es2021: true,
-      node: true,
-    },
-    extends: 'eslint:recommended',
-    parserOptions: {
-      ecmaVersion: 12,
-    },
-    rules: {
-      // Añade reglas específicas si las necesitas
-    },
-  };
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
