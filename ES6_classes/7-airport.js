77859e4ebbdb3c728d66f6df5d471ea45eddc0d1
Fabri <oviedofabrizzio1998@gmail.com>
2025-03-30 18:05:02 -0300

    Add ES6 classes for currency, pricing, building, and airport management

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,13 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string' || typeof code !== 'string') {
+      throw new TypeError('values must be strings');
+    }
+    this._name = name;
+    this._code = code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
