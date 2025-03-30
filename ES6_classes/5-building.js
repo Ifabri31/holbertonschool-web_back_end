965534254b2d992f840e6166b7add732f911f233
Fabri <oviedofabrizzio1998@gmail.com>
2025-03-30 18:08:06 -0300

    Refactor condition in Building class to improve readability

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -6,9 +6,7 @@ export default class Building {
       throw new TypeError('sqft must be a number');
     }
 
-    if (
-      typeof this.evacuationWarningMessage !== 'function' &&
-      this.constructor !== Building
+    if (typeof this.evacuationWarningMessage !== 'function' && this.constructor !== Building
     ) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
